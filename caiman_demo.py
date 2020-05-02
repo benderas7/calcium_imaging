c87fb38094f4f5eb4ee1f5fd7d9708cb4126bfa2
Andrew Bender <andrew.bender710@gmail.com>
2020-05-02 16:58:26 -0700

    Set up all functions

---
+++ caiman_demo.py
@@ -0,0 +1,95 @@
+"""Largely based off of CaImAn's demo_pipeline.ipynb found here:
+https://github.com/flatironinstitute/CaImAn/blob/master/demos/notebooks"""
+# Import necessary modules
+import bokeh.plotting as bpl
+import cv2
+import glob
+import logging
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import caiman as cm
+from caiman.motion_correction import MotionCorrect
+from caiman.source_extraction.cnmf import cnmf as cnmf
+from caiman.source_extraction.cnmf import params as params
+from caiman.utils.utils import download_demo
+from caiman.utils.visualization import plot_contours, nb_view_patches,\
+    nb_plot_contour
+
+# Set up constants
+
+
+def set_up_logger():
+    return
+
+
+def play_movie():
+    return
+
+
+def setup_params():
+    return
+
+
+def create_params_obj():
+    return
+
+
+def motion_corr():
+    return
+
+
+def mem_mapping():
+    return
+
+
+def run_cnmf():
+    return
+
+
+def run_pipeline():
+    return
+
+
+def inspect_results():
+    return
+
+
+def rerun_cnmf():
+    return
+
+
+def comp_eval():
+    return
+
+
+def extract_df_over_f():
+    return
+
+
+def sel_hq_comps():
+    return
+
+
+def disp_results():
+    return
+
+
+def save_results():
+    return
+
+
+def clean_log():
+    return
+
+
+def view_results_movie():
+    return
+
+
+def main():
+    return
+
+
+if __name__ == '__main__':
+    main()
