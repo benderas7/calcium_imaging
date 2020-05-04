02e571a9f34cdb1804dff65a4713ae444413835a
Andrew Bender <andrew.bender710@gmail.com>
2020-05-03 17:41:34 -0700

    Make backbone for images to video

---
+++ video_from_images.py
@@ -0,0 +1,23 @@
+# Import necessary modules
+import cv2
+import os
+
+# Set constants
+IMAGES_DIR = 'data/N.01.01/images'
+VIDEO_FN = 'data/N.01.01/video.tif'
+
+
+def load_images():
+    return
+
+
+def write_video():
+    return
+
+
+def main():
+    return
+
+
+if __name__ == '__main__':
+    main()
