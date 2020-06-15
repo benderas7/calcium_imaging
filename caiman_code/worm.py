396a820d20f40d9d19fa38ca4594f7418087d7d1
Andrew Bender <andrew.bender710@gmail.com>
2020-06-15 13:07:42 -0700

    Parse filenames for t and z

--- caiman_code/worm.py
+++ caiman_code/worm.py
@@ -1,6 +1,8 @@
 # Import necessary modules
 import caiman_code.funcs
 import logging
+import os
+import re
 
 # Logging parameters
 LOG = True
@@ -8,50 +10,38 @@ LOG_FN = '/tmp/caiman.log'
 LOG_LEVEL = logging.WARNING
 
 # Data and data display parameters
-VIDEO_FN = '../data/NeuroPAL/11.25.19/worm3_gcamp_Out/' \
-           'worm3_gcamp_Out_t001z01_ORG.tif'
+VIDEO_DIR = 'data/NeuroPAL/11.25.19/worm3_gcamp_Out'
 DISP_MOVIE = True
 SAVE_RESULTS_DIR = '.'
 
 # Dataset dependent parameters
 FR = 30  # imaging rate in frames per second
 DECAY_TIME = 0.4  # length of a typical transient in seconds
-
-# Motion correction parameters
-STRIDES = (48, 48)  # start a new patch for pw-rigid motion correction every
-# x pixels
-OVERLAPS = (24, 24)  # overlap between pathes (size of patch strides+overlaps)
-MAX_SHIFTS = (6, 6)  # maximum allowed rigid shifts (in pixels)
-MAX_DEV_RIGID = 3  # maximum shifts deviation allowed for patch with
-# respect to rigid shifts
-PW_RIGID = True  # flag for performing non-rigid motion correction
-
-# Parameters for source extraction and deconvolution
-P = 1  # order of the autoregressive system
-GNB = 2  # number of global background components
-MERGE_THR = 0.85  # merging threshold, max correlation allowed
-RF = 15  # half-size of the patches in pixels. e.g., if rf=25, patches are
-# 50x50
-STRIDE_CNMF = 6  # amount of overlap between the patches in pixels
-K = 4  # number of components per patch
-GSIG = [4, 4]  # expected half size of neurons in pixels
-METHOD_INIT = 'greedy_roi'  # initialization method (if analyzing dendritic
-# data using 'sparse_nmf')
-SSUB = 1  # spatial subsampling during initialization
-TSUB = 1  # temporal subsampling during intialization
-
-# Parameters for component evaluation
-MIN_SNR = 2.0  # signal to noise ratio for accepting a component
-RVAL_THR = 0.85  # space correlation threshold for accepting a component
-CNN_THR = 0.99  # threshold for CNN based classifier
-CNN_LOWEST = 0.1  # neurons with cnn probability lower than this value are
-# rejected
 ######
 
 
-def run(video_fn=VIDEO_FN, log=LOG, log_fn=LOG_FN, log_level=LOG_LEVEL,
+def compile_vids_to_arr(video_dir, t_char='t', z_char='z'):
+    # Make sure video directory is in fact a directory
+    assert os.path.isdir(video_dir)
+
+    # Compile videos
+    for fn in [f for f in os.listdir(video_dir) if f.endswith('.tif')]:
+        # Parse filename to get t and z - *very specific to worm files*
+        tz_str = fn.split('_')[3]
+        t = re.findall('\d+', tz_str[tz_str.index(t_char):])[0]
+        z = re.findall('\d+', tz_str[tz_str.index(z_char):])[0]
+
+    arr = []
+    return arr
+
+
+def run(video_dir=VIDEO_DIR, log=LOG, log_fn=LOG_FN, log_level=LOG_LEVEL,
         fr=FR, decay_time=DECAY_TIME, disp_movie=DISP_MOVIE,
         save_results_dir=SAVE_RESULTS_DIR):
+    # Compile videos into array
+    video_fn = compile_vids_to_arr(video_dir)
+
+    # Run pipeline
     caiman_code.funcs.pipeline(video_fn, log, log_fn, log_level, fr,
                                decay_time, disp_movie, save_results_dir)
     return
