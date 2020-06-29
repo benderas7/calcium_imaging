7a63c8e067aaa854438eb2b9ffe7d8d07b9b5dba
Andrew Bender <andrew.bender710@gmail.com>
2020-06-29 15:52:10 -0700

    Make movie params user-dependent

--- caiman_code/quality_assessment.py
+++ caiman_code/quality_assessment.py
@@ -6,6 +6,11 @@ import os
 from caiman.source_extraction.cnmf import cnmf
 import caiman_code.funcs as funcs
 from caiman_code.worm import COMPILED_DIR
+
+# Set parameters
+DISP_MOVIE = False
+COLOR_COMPS = True
+SAVE_NAME = 'results_movie.avi'
 #####
 
 
@@ -18,24 +23,27 @@ def load_results(results_dir):
     return cnm
 
 
-def make_movie(cnm):
+def make_movie(cnm, disp_movie, color_comps, save_fn):
     # Get images from load memmap
     images = funcs.load_memmap(cnm.mmap_file)
 
     # Make video for each ROI
     mov = cnm.estimates.play_movie(
-        images, display=False, use_color=True, save_movie=True)
+        images, display=disp_movie, use_color=color_comps,
+        save_movie=bool(save_fn), movie_name=save_fn)
     return mov
 
 
-def main():
+def main(results_dir=COMPILED_DIR, disp_movie=DISP_MOVIE,
+         color_comps=COLOR_COMPS, save_name=SAVE_NAME):
     # Load results
-    cnm = load_results(results_dir='..')
+    cnm = load_results(results_dir)
 
     # Make movie
-    make_movie(cnm)
+    save_fn = os.path.join(results_dir, save_name)
+    make_movie(cnm, disp_movie, color_comps, save_fn)
     return
 
 
 if __name__ == '__main__':
-    main()
+    main(results_dir='..')
