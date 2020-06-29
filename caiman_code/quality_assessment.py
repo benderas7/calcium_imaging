1e9567018261a658ccfee47aa77d283522dd08fe
Andrew Bender <andrew.bender710@gmail.com>
2020-06-29 15:23:11 -0700

    Load results with native CaImAn function

--- caiman_code/quality_assessment.py
+++ caiman_code/quality_assessment.py
@@ -2,23 +2,30 @@
 @author:benderas7
 """
 # Import necessary modules
-import h5py
 import os
-
+from caiman.source_extraction.cnmf import cnmf
+import cv2
+import matplotlib.pyplot as plt
 from caiman_code.worm import COMPILED_DIR
 
 #####
 
 
 def load_results(results_dir):
+    # Determine filename from dir
     fn = os.path.join(results_dir, 'analysis_results.hdf5')
-    h5f = h5py.File(fn, 'r')
-    return h5f['estimates']
+
+    # Load CNMF object using CaImAn function
+    cnm = cnmf.load_CNMF(fn)
+    return cnm
 
 
-def make_video_roi():
+def main():
+    # Load results
+    cnm = load_results(results_dir='..')
     return
 
 
 if __name__ == '__main__':
-    load_results(results_dir='..')
+
+
